c05e2e71df806dd77897ececd9bde39c62b2e5ce
Grzegorz Czmielewski <grzegorz.czmielewski@etteplan.com>
2021-03-17 16:16:05 +0100

    added swagger config and some basic routes definition

--- src/routes/club.ts
+++ src/routes/club.ts
@@ -9,6 +9,15 @@ const route = Router();
 export default (app: Router): void => {
   app.use('/club', route);
 
+  /**
+   * @swagger
+   * /club/members:
+   *  get:
+   *    description: Get club members
+   *    responses:
+   *      200:
+   *        description: Success
+   */
   route.get('/members', async (req: Request, res: Response) => {
     const clubCotrollerInstance = Container.get(ClubController);
     const logger: Logger = Container.get('logger');
@@ -20,7 +29,21 @@ export default (app: Router): void => {
     }
   });
 
-  // Query e.g. http://localhost:3000/club/activities?keyA=valA
+  /**
+   * @swagger
+   * /club/activities:
+   *  get:
+   *    description: Get club activities
+   *    parameters:
+   *      - name: type
+   *        in: query
+   *        description: Activity type
+   *        type: string
+   *        required: false
+   *    responses:
+   *      200:
+   *        description: Return club members sport activities.
+   */
   route.get('/activities', async (req: Request, res: Response) => {
     const clubCotrollerInstance = Container.get(ClubController);
     const logger: Logger = Container.get('logger');
