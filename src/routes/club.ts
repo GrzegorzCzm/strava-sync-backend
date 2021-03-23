a82d127be3317a4cb42d8971ecfe993502493be5
Grzegorz <32096835+GrzegorzCzm@users.noreply.github.com>
2021-03-23 20:48:58 +0100

    Swagger api definition #4 (#9)
    * added swagger config and some basic routes definition
    
    * defined query params to get club activities
    
    * define schemas for exposed routes
    
    * add ref in api definition

--- src/routes/club.ts
+++ src/routes/club.ts
@@ -4,11 +4,71 @@ import { Logger } from 'winston';
 
 import ClubController from '../controllers/clubController';
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    ActivityType:
+ *      type: array
+ *      items:
+ *        type: string
+ *        enum: [AlpineSki, BackcountrySki, Canoeing, Crossfit, EBikeRide, Elliptical, Golf, Handcycle, Hike, IceSkate,
+ *            InlineSkate, Kayaking, Kitesurf, NordicSki, Ride, RockClimbing, RollerSki, Rowing, Run, Sail, Skateboard,
+ *            Snowboard, Snowshoe, Soccer, StairStepper, StandUpPaddling, Surfing, Swim, Velomobile, VirtualRide, VirtualRun,
+ *            Walk, WeightTraining, Wheelchair, Windsurf, Workout, Yoga]
+ *    ClubMembers:
+ *      type: array
+ *      items:
+ *        type: string
+ *    Activity:
+ *      properties:
+ *        date:
+ *          type: number
+ *          description: Date in unixTimestamp
+ *        id:
+ *          type: string
+ *          description: Activity ID
+ *        athlete:
+ *          type: string
+ *          description: Athlete name
+ *        distance:
+ *          type: number
+ *          description: Distance in meters
+ *        movingTime:
+ *          type: number
+ *          description: Activitiy duration in seconds
+ *        name:
+ *          type: string
+ *          description: Activity name
+ *        type:
+ *          schema:
+ *            $ref: '#/components/schemas/ActivityType'
+ *
+ *
+ */
+
 const route = Router();
 
 export default (app: Router): void => {
   app.use('/club', route);
 
+  /**
+   * @swagger
+   * /club/members:
+   *  get:
+   *    tags:
+   *      - name: club
+   *    description: Get club members
+   *    operationId: getClubMembers
+   *    responses:
+   *      200:
+   *        description: Success
+   *        content:
+   *          application/json:
+   *            schema:
+   *             $ref: '#/components/schemas/ClubMembers'
+   *            example: [Jan_S, Grzegorz_C]
+   */
   route.get('/members', async (req: Request, res: Response) => {
     const clubCotrollerInstance = Container.get(ClubController);
     const logger: Logger = Container.get('logger');
@@ -20,7 +80,85 @@ export default (app: Router): void => {
     }
   });
 
-  // Query e.g. http://localhost:3000/club/activities?keyA=valA
+  /**
+   * @swagger
+   * /club/activities:
+   *  get:
+   *    tags:
+   *      - name: club
+   *    description: Get club activities
+   *    operationId: getClubActivities
+   *    parameters:
+   *      - name: type
+   *        in: query
+   *        description: Activity type. E.g. Run, Walk, etc...
+   *        type: string
+   *        required: false
+   *        style: form
+   *        schema:
+   *          $ref: '#/components/schemas/ActivityType'
+   *        collectionFormat: multi
+   *      - name: athlete
+   *        in: query
+   *        description: Athlete name
+   *        style: form
+   *        schema:
+   *          type: array
+   *          items:
+   *            type: string
+   *        collectionFormat: multi
+   *        required: false
+   *      - name: name
+   *        in: query
+   *        description: Activity name
+   *        type: string
+   *        required: false
+   *        style: form
+   *        schema:
+   *          type: array
+   *          items:
+   *            type: string
+   *        collectionFormat: multi
+   *      - name: dateFrom
+   *        in: query
+   *        description: Activities which was created after this date. E.g 2020-01-01
+   *        type: date
+   *        required: false
+   *      - name: dateTo
+   *        in: query
+   *        description: Activities which was created before or at this date. E.g. 2020-01-01
+   *        type: date
+   *        required: false
+   *      - name: movingFrom
+   *        in: query
+   *        description: Activities which was >= duration given in seconds
+   *        type: number
+   *        required: false
+   *      - name: movingTo
+   *        in: query
+   *        description: Activities which was <=  duration given in seconds
+   *        type: number
+   *        required: false
+   *      - name: distanceFrom
+   *        in: query
+   *        description: Activities which was >= distance given in meters
+   *        type: number
+   *        required: false
+   *      - name: distanceTo
+   *        in: query
+   *        description: Activities which was <=> given in meters
+   *        type: number
+   *        required: false
+   *    responses:
+   *      200:
+   *        description: Return club members sport activities.
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: array
+   *              items:
+   *                $ref: '#/components/schemas/Activity'
+   */
   route.get('/activities', async (req: Request, res: Response) => {
     const clubCotrollerInstance = Container.get(ClubController);
     const logger: Logger = Container.get('logger');
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,27 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
 import club from './club';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Strava sync',
+      version: '0.0.0',
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+};
 
 export default (): Router => {
   const app = Router();
+
+  const swaggerSpec = swaggerJSDoc(options);
+  app.use('/api-docs', swaggerUi.serve);
+  app.get('/api-docs', swaggerUi.setup(swaggerSpec));
+
   club(app);
 
   return app;
